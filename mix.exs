6137e30b8c85e22adf7016f6cb665365cd106d50
Mengxuan Xia <xiamx2004@gmail.com>
2017-07-27 22:44:16 -0400

    release v0.4.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GenFST.Mixfile do
 
   def project do
     [app: :gen_fst,
-     version: "0.4.0",
+     version: "0.4.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
